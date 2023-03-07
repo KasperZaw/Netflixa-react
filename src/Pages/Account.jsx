0b55b03497a340fd0197a19fb910f811ac5d8ee1
KasperZaw <119044330+KasperZaw@users.noreply.github.com>
2023-03-07 19:17:30 +0100

    user

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import '../Styles/account.css'
 import { useState, useEffect } from 'react'
 const Account = () => {
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+
+  const handleAvatarClick = (avatar) => {
+    setSelectedAvatar(avatar);
+  };
+
+  
   useEffect(() => {
      const closeBtn = document.getElementById('close-btn');
      const iconsContainer = document.querySelector('.icons')
@@ -22,32 +29,58 @@ const Account = () => {
     const avatars = document.querySelectorAll('#avatar');
     const avatarContainer = document.getElementById('avatar-container');
   
-   
+    avatars.forEach((avatar) => {
+      avatar.addEventListener('click', () => {
+        avatarContainer.innerHTML = avatar.outerHTML;
+      });
+    });
   }, []);
-  
    
 
 
   
   return (
     <div className='main'>
-        <div className='icons'>
+      <div className='icons'>
         <i className="fa-sharp fa-regular fa-circle-xmark" id='close-btn'></i>
         <div className='icons-container'>
-          <img src={require("../img/Netflix-avatar-2.png" )}alt="profile" id='avatar'/>
-          <img src={require("../img/Netflix-avatar-3.png" )}alt="profile" id='avatar'/>
-          <img src={require("../img/Netflix-avatar-4.png" )}alt="profile" id='avatar'/>
-          <img src={require("../img/Netflix-avatar-5.png" )}alt="profile" id='avatar'/>
-          <img src={require("../img/Netflix-avatar-6.png" )}alt="profile" id='avatar'/>
+          <img
+            src={require("../img/Netflix-avatar-2.png")}
+            alt="profile"
+            onClick={() => handleAvatarClick("Netflix-avatar-2.png")}
+          />
+          <img
+            src={require("../img/Netflix-avatar-3.png")}
+            alt="profile"
+            onClick={() => handleAvatarClick("Netflix-avatar-3.png")}
+          />
+          <img
+            src={require("../img/Netflix-avatar-4.png")}
+            alt="profile"
+            onClick={() => handleAvatarClick("Netflix-avatar-4.png")}
+          />
+          <img
+            src={require("../img/Netflix-avatar-5.png")}
+            alt="profile"
+            onClick={() => handleAvatarClick("Netflix-avatar-5.png")}
+          />
+          <img
+            src={require("../img/Netflix-avatar-6.png")}
+            alt="profile"
+            onClick={() => handleAvatarClick("Netflix-avatar-6.png")}
+          />
         </div>
-        </div>
+      </div>
       <div className='img-wrapper' id='main-img'>
-        <img src={require("../img/Netflix-avatar.png")} alt="profile" id='avatar-container'/>
+        {selectedAvatar ? (
+          <img src={require(`../img/${selectedAvatar}`)} alt="profile" />
+        ) : (
+          <img src={require("../img/Netflix-avatar.png")} alt="profile" />
+        )}
         <i className="fa-solid fa-pen"></i>
-        </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
 export default Account
