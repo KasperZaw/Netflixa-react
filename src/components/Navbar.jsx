06569865bb251c261d05fefd1603b54f9b1b1ee3
KasperZaw <119044330+KasperZaw@users.noreply.github.com>
2023-03-07 16:27:32 +0100

    working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,12 @@ import { useState } from 'react';
 import { Link, useNavigate} from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 const Navbar = () =>{
-  const [searchQuery, setSearchQuery] = useState('');
+
   const [isScrolled, setIsScrolled] = useState(false);
   const {user, logOut} = UserAuth();
   const navigate = useNavigate();
   
-  const handleSearchQueryChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-  
+
   const handleLogout = async () => {
     try {
       await logOut()
@@ -35,14 +32,7 @@ const Navbar = () =>{
     };
   }, []);
 
-  const handleSearchButtonClick = async () => {
-    // fetch movies data based on the search query
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=0cbff6ee7af9c9e655659e7dcdc0dcef&query=${searchQuery}`);
-    const data = await response.json();
-    // navigate to the search results page
-    navigate(`/search/${searchQuery}`, { state: { movies: data.results } });
-  };
-  
+
   return (
     <nav className={isScrolled ? 'scrolled' : ''}>
         <Link to='/' style={{ textDecoration: 'none' }}> 
@@ -50,15 +40,7 @@ const Navbar = () =>{
         </Link>
        {user?.email ? ( 
          <div className='buttons-container'>
-         <div className='search-container'>
-            <input
-            className='search'
-            type="text" 
-            value={searchQuery}
-            onChange={handleSearchQueryChange}
-            />
-            <i className="fa-sharp fa-solid fa-magnifying-glass"  onClick={handleSearchButtonClick}></i>
-         </div>
+        
             <Link to='account'><img src={require("../img/Netflix-avatar.png")} alt="" /></Link>
             <button className='btn-2' onClick={handleLogout}>Logout</button>
         </div>) : (
